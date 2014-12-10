b58663c50c0c88a65b9e661b25057d97002b56d7
sylar.wang@foxmail.com <sylar.wang@foxmail.com>
2014-12-11 00:41:18 +0800

    spring xml 注入方式单元测试

--- springsample/src/test/java/com/achuan/springsample/BeanOfXmlTests.java
+++ springsample/src/test/java/com/achuan/springsample/BeanOfXmlTests.java
@@ -16,11 +16,13 @@ public class BeanOfXmlTests extends TestBase {
 
 	@Test
 	public void testBean() {
+		// 默认构造函数bean对象，由于没有注入任何值，所以都为null
 		BeanOfXml bean = ctx.getBean("bean", BeanOfXml.class);
 		assertNotNull(bean);
 		assertNull(bean.getUserBean());
 		assertNull(bean.getMessage());
 
+		// 属性注入bean对象，都有值；
 		BeanOfXml beanInjectByProperty = ctx.getBean("beanInjectByProperty", BeanOfXml.class);
 		assertNotNull(beanInjectByProperty);
 		assertNotNull(beanInjectByProperty.getMessage());
@@ -29,6 +31,7 @@ public class BeanOfXmlTests extends TestBase {
 		assertEquals(1, beanInjectByProperty.getUserBean().getUserId());
 		assertEquals("beimai", beanInjectByProperty.getUserBean().getUserName());
 
+		// 构造函数注入bean
 		BeanOfXml beanInjectByConstructor = ctx.getBean("beanInjectByConstructor", BeanOfXml.class);
 		assertNotNull(beanInjectByConstructor);
 		assertNotNull(beanInjectByConstructor.getMessage());
